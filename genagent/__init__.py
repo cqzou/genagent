db8eff542146f3e233f88be32e0619e2ecde61ca
Carolyn Zou <cqz@cs.stanford.edu>
2025-05-27 15:57:43 -0700

    pypi ver

---
+++ genagent/__init__.py
@@ -0,0 +1,18 @@
+from .llm_utils import (
+    gen,
+    simple_gen,
+    fill_prompt,
+    make_output_format,
+    modular_instructions,
+    parse_json,
+    mod_gen,
+    get_embedding,
+    get_image,
+    MemoryNode,
+    Agent,
+    create_simple_agent,
+    ChatSession,
+    create_simple_chat
+)
+
+__version__ = "0.1.1"
